4555b0d148126d292611df9e5d7265d97fd9554a
rafaelEstevam <rafael.estevamdesign@gmail.com>
2024-03-20 15:12:23 -0300

    deploy

--- recados-io-front/src/config/pusher.ts
+++ recados-io-front/src/config/pusher.ts
@@ -4,7 +4,7 @@ const pusher = new Pusher(
   '1c458ef2083750c2ffa9',
   {
     cluster: 'us2',
-    authEndpoint: 'http://localhost:8888/pusher/auth'
+    authEndpoint: `${process.env.VUE_APP_API}/pusher/auth`
   }
 );
 
